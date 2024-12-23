fe91f9305600ac42b1f5ede157df686f5ed9558f
LuisLgvc <luislgvc21@gmail.com>
2024-12-23 00:53:05 -0300

    fix error SECRET_KEY

--- Vivero/settings.py
+++ Vivero/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY'), default='your_secret_key'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
